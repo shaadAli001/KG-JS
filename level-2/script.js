9abba2f11aa66a1cac157c638c5bc0308332a37d
shaadAli001 <alishaad908@gmail.com>
2025-11-13 16:21:51 +0530

    pending

---
+++ level-2/script.js
@@ -0,0 +1,21 @@
+// let msg = "Hello";
+// console.log(msg);
+
+// let str = "Current IPO";
+// let price = 144897.35;
+// let profit = 45 / 100;
+// let calc = Math.round(price + price * profit);
+// let Amount = `${str}= ₹ ${calc}`;
+
+// console.log(Amount);
+
+// let i = 1;
+// i += 5;
+// console.log(i);
+// i -= 5;
+// console.log(i);
+// i /= 5;
+// console.log(i);
+// i *= 5;
+// console.log(i);
+
