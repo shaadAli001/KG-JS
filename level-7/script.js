5b9016639830713a02b17f41ea4f8cba7db3a766
shaadali001 <alishaad908@gmail.com>
2025-11-21 23:48:23 +0530

    loop & Array

---
+++ level-7/script.js
@@ -0,0 +1,41 @@
+// Array
+// let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// console.log(Array.isArray(a));
+// Push
+// a.push(11);
+// console.log(a);
+
+// pop
+// a.unshift()
+// console.log(a);
+// console.log('Poped Elememt' + a.pop());
+// console.log(a);
+
+// Splice
+// a.splice(2,4)
+// console.log(a);
+// String
+// console.log(a.toString());
+
+// Sort
+// let arr=[9,4,6,1,3,5,0]
+// console.log(arr.sort());
+// console.log(arr.valueOf());
+
+// WhileLoop
+let num = 1;
+// while (num <= 10) {
+//     console.log(num);
+//     num++;
+// }
+
+// Do while
+// do {
+//     console.log(num);
+//     num++
+// } while (num <= 20);
+
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
